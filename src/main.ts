82b7e9874853c6363e21711b23c1c0f587c6fbf8
shaper <15519586771@163.com>
2022-05-09 21:19:20 +0800

    feat: 添加路由

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import plugins from "@/lib/plugins";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(...plugins)
+  .mount("#app");
