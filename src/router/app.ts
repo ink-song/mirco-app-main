e5440807ad26f1f2b9bb2bd83ab1d3f387b71700
shaper <15519586771@163.com>
2022-05-09 11:26:14 +0800

    feat: init

---
+++ src/router/app.ts
@@ -0,0 +1,19 @@
+import { RouteRecordRaw } from "vue-router";
+
+import Home from "../views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
+
+export default routes;
